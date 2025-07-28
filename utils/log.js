1280bf8eb3c5655e91b8ea53af227646daaaae38
raselmahmud5 <calabobot2@gmail.com>
2025-07-28 18:10:18 +0600

    Create log.js

---
+++ utils/log.js
@@ -0,0 +1,42 @@
+const chalk = require("chalk");
+
+function logger(content, type = "INFO") {
+  const time = new Date().toLocaleTimeString("bn-BD", { hour12: true });
+
+  let tagColor;
+
+  switch (type.toUpperCase()) {
+    case "ERROR":
+      tagColor = chalk.red;
+      break;
+    case "STARTING":
+      tagColor = chalk.greenBright;
+      break;
+    case "REPO":
+    case "PROFILE":
+    case "UID INFO":
+    case "BOT":
+      tagColor = chalk.magentaBright;
+      break;
+    case "DEVELOPER":
+      tagColor = chalk.blueBright;
+      break;
+    case "VERSION":
+    case "NAME":
+    case "DESCRIPTION":
+      tagColor = chalk.yellowBright;
+      break;
+    case "RESTARTING":
+      tagColor = chalk.cyanBright;
+      break;
+    case "STOPPED":
+      tagColor = chalk.gray;
+      break;
+    default:
+      tagColor = chalk.white;
+  }
+
+  console.log(chalk.gray(`[ ${time} ]`), tagColor(`[ ${type} ]`), chalk.white(content));
+}
+
+module.exports = logger;
