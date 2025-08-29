93380c4f1b00a15e9340dd899fcdc91351ff14b9
raselmahmud5 <calabobot2@gmail.com>
2025-08-29 18:44:58 +0600

    Update handleCommand.js

--- includes/handle/handleCommand.js
+++ includes/handle/handleCommand.js
@@ -6,7 +6,7 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
   const moment = require("moment-timezone");
   return async function ({ event }) {
     const dateNow = Date.now()
-    const time = moment.tz("Asia/Kolkata").format("HH:MM:ss DD/MM/YYYY");
+    const time = moment.tz("Asia/Dhaka").format("HH:MM:ss DD/MM/YYYY");
     const { allowInbox, PREFIX, ADMINBOT, NDH, DeveloperMode, adminOnly, keyAdminOnly, ndhOnly,adminPaOnly } = global.config;
     const { userBanned, threadBanned, threadInfo, threadData, commandBanned } = global.data;
     const { commands, cooldowns } = global.client;
@@ -27,7 +27,7 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
     if (!NDH.includes(senderID) && !ADMINBOT.includes(senderID) && adminbot.ndhOnly == true) {
       if (!NDH.includes(senderID) && !ADMINBOT.includes(senderID) && adminbot.ndhOnly == true) return api.sendMessage('MODE » Only bot support can use bots', threadID, messageID)
     }
-    const dataAdbox = require('../../Priyansh/commands/cache/data.json');
+    const dataAdbox = require('../../Script/commands/cache/data.json');
     var threadInf = (threadInfo.get(threadID) || await Threads.getInfo(threadID));
     const findd = threadInf.adminIDs.find(el => el.id == senderID);
     if (dataAdbox.adminbox.hasOwnProperty(threadID) && dataAdbox.adminbox[threadID] == true && !ADMINBOT.includes(senderID) && !findd && event.isGroup == true) return api.sendMessage('MODE » Only admins can use bots', event.threadID, event.messageID)
